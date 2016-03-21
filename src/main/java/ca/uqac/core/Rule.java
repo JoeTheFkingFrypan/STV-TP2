42cf1343f13759ef9943b2f252ab61a9fcd35dfd
Romain GAGNAIRE <romgagnaire@gmail.com>
2016-03-21 19:54:02 -0400

    - [ADDITION] Rule can rule out possible pair if number of constraint is exactly 2
    * validateParameters should only be called if there's exactly to
    constraints
    * This state is enforced by using preconditions (from Guava library)

--- src/main/java/ca/uqac/core/Rule.java
+++ src/main/java/ca/uqac/core/Rule.java
@@ -1,5 +1,7 @@
 package ca.uqac.core;
 
+import ca.uqac.validation.ParameterValue;
+import com.google.common.base.Preconditions;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
@@ -16,12 +18,21 @@ public class Rule {
         this.constraintList.add(c);
     }
 
-    public boolean validateQuery(final String query) {
-        return false;
+    public boolean validateParameters(final ParameterValue lhs, final ParameterValue rhs) {
+        Preconditions.checkState(this.forbidPair());
+
+        Constraint c1 = this.constraintList.get(0);
+        Constraint c2 = this.constraintList.get(1);
+
+        return !( (c1.match(lhs) && c2.match(rhs)) || (c2.match(lhs) && c1.match(rhs)) );
     }
 
     @Override
     public String toString() {
         return "[RULE] " + StringUtils.join(this.constraintList, " & ");
+    }
+
+    public boolean forbidPair() {
+        return this.constraintList.size() == 2;
     }
 }
