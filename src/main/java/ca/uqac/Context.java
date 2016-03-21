1a39df5fe458ff03fba507acd2caebfdf6f5883d
Romain GAGNAIRE <romgagnaire@gmail.com>
2016-03-21 19:55:38 -0400

    - [WIP] First partial prototype towards test cases generation

--- src/main/java/ca/uqac/Context.java
+++ src/main/java/ca/uqac/Context.java
@@ -2,17 +2,22 @@ package ca.uqac;
 
 import ca.uqac.core.Option;
 import ca.uqac.core.Rule;
+import ca.uqac.validation.Pair;
+import ca.uqac.validation.ParameterValue;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Context {
+    private List<Pair> availablePairs;
     private List<Option> options;
     private List<Rule> rules;
     private int optionValues;
+    private List<String> parameterOrdering;
 
     public Context() {
+        this.availablePairs = new ArrayList<>();
         this.options = new ArrayList<>();
         this.rules = new ArrayList<>();
         this.optionValues = 0;
@@ -27,11 +32,50 @@ public class Context {
         this.rules.add(rule);
     }
 
-    public int getNumberOfParameters() {
-        return this.options.size();
+    public List<Pair> getPairs() {
+        if(!this.availablePairs.isEmpty()) {
+            return this.availablePairs;
+        }
+
+        System.out.println("- Pair generation has begun");
+        final int numberOfParameters = this.options.size();
+        for(int i=0; i < numberOfParameters-1; ++i) {
+            for(int j=i+1; j < numberOfParameters; ++j) {
+
+                Option lhs = this.options.get(i);
+                Option rhs = this.options.get(j);
+
+                for(int x=0; x < lhs.getNumberOfPossibleValues(); x++) {
+                    for(int y=0; y < rhs.getNumberOfPossibleValues(); y++) {
+
+                        final ParameterValue currentLhs = new ParameterValue(lhs.getKey(), lhs.getValueAt(x));
+                        final ParameterValue currentRhs = new ParameterValue(rhs.getKey(), rhs.getValueAt(y));
+
+                        if(pairNotForbidden(currentLhs, currentRhs)) {
+                            this.availablePairs.add(new Pair(lhs.getKey(), lhs.getValueAt(x), rhs.getKey(), rhs.getValueAt(y)));
+                        } else {
+                            System.out.println("- Pair discarded for not complying to rules: " + currentLhs + " & " + currentRhs);
+                        }
+                    }
+                }
+            }
+        }
+
+        return this.availablePairs;
     }
 
-    public int getNumberOfPairs() {
+    private boolean pairNotForbidden(final ParameterValue lhs, final ParameterValue rhs) {
+        for(Rule rule: this.rules) {
+            if(rule.forbidPair()) {
+                if(!rule.validateParameters(lhs, rhs)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private int getTotalNumberOfPairs() {
         int numberOfPairs = 0;
         final int numberOfParameters = this.options.size();
         for(int i=0; i < numberOfParameters-1; ++i) {
@@ -42,11 +86,19 @@ public class Context {
         return numberOfPairs;
     }
 
+    public int getNumberOfParameterValues() {
+        return this.optionValues;
+    }
+
+    public int getNumberOfParameters() {
+        return this.options.size();
+    }
+
     public void displayInfo() {
         System.out.println("\n============== INFO ==============");
         System.out.println("- There are " + this.options.size() + " parameters/options.");
         System.out.println("- There are " + this.optionValues + " parameter values.");
-        System.out.println("- There are " + this.getNumberOfPairs() + " possible pairs (without taking into account rules).");
+        System.out.println("- There are " + this.getTotalNumberOfPairs() + " possible pairs (without taking into account rules).");
         System.out.println("- There are " + this.rules.size() + " rules.");
         System.out.println(this.toString());
     }
@@ -61,5 +113,23 @@ public class Context {
             sb.append(StringUtils.join(this.rules, "\n"));
         }
         return sb.toString();
+    }
+
+    public List<String> getParameterPositions() {
+        List<String> positions = new ArrayList<>();
+        for (Option option : this.options) {
+            for (int j = 0; j < option.getNumberOfPossibleValues(); ++j) {
+                positions.add(option.getKey());
+            }
+        }
+        return positions;
+    }
+
+    public List<String> getParameterOrdering() {
+        List<String> ordering = new ArrayList<>();
+        for (Option option : this.options) {
+            ordering.add(option.getKey());
+        }
+        return ordering;
     }
 }
---
+++ src/main/java/ca/uqac/validation/TestCaseGenerator.java
@@ -0,0 +1,96 @@
+package ca.uqac.validation;
+
+import ca.uqac.Context;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestCaseGenerator {
+
+    private final Context context;
+    private final List<Pair> testCases;
+    private List<Pair> unusedPairs;
+    private List<Pair> availablePairs;
+    private UnusedCounts unusedCounts;
+
+    public TestCaseGenerator(final Context context) {
+        this.testCases = new ArrayList<>();
+        this.context = context;
+        context.displayInfo();
+        this.processPairs();
+        this.computeTestCases();
+    }
+
+    private void processPairs() {
+        this.availablePairs = this.context.getPairs();
+        this.unusedPairs = new ArrayList<>(this.availablePairs);
+
+        System.out.println("\n============== PAIRS ==============");
+        System.out.println("- There are " + this.availablePairs.size() + " pairs that complies to rules");
+    }
+
+    private void computeTestCases() {
+
+        /*List<String> parameterPositions = this.context.getParameterPositions();
+        for (int i = 0; i < parameterPositions.size(); ++i) {
+            System.out.println("POSITION [" + i + "] " + parameterPositions.get(i));
+        }
+
+        List<String> ordering = this.context.getParameterOrdering();
+        for (int i = 0; i < ordering.size(); ++i) {
+            System.out.println("ORDERING [" + i + "] " + parameterPositions.get(i));
+        }*/
+
+        this.unusedCounts = new UnusedCounts();
+        for(Pair p: this.availablePairs) {
+            this.unusedCounts.increment(p.getLhsParameterValue(), p.getRhsParameterValue());
+        }
+
+        Pair best = this.findBestPair();
+        this.testCases.add(best);
+        System.out.println("[DEBUG] Best pair should be: " + best);
+
+        /*int firstPos = parameterPositions.indexOf(best.getLhsParameterKey()); // position of first value from best unused pair
+        int secondPos = parameterPositions.indexOf(best.getRhsParameterKey()); // position of first value from best unused pair
+        Collections.swap(ordering, firstPos, secondPos);
+        for (int i = 0; i < ordering.size(); ++i) {
+            System.out.println("NEW ORDERING [" + i + "] " + parameterPositions.get(i));
+        }
+
+        for(int i=2; i<this.context.getNumberOfParameters(); i++) {
+
+        }*/
+
+
+    }
+
+    private Pair findBestPair() {
+        // pick "best" unusedPair -- the pair which has the sum of the most unused values
+        int bestWeight = 0;
+        int indexOfBestPair = 0;
+        for (int i = 0; i < this.unusedPairs.size(); ++i) {
+            Pair p = this.unusedPairs.get(i);
+            int weight = this.unusedCounts.getWeightFrom(p.getLhsParameterValue(), p.getRhsParameterValue());
+            //System.out.println("[DEBUG]     Weight for " + i + "th(" + p +") is " + weight);
+            if (weight > bestWeight) {
+                bestWeight = weight;
+                indexOfBestPair = i;
+            }
+        }
+        System.out.println("[DEBUG] Index from best pair: " + indexOfBestPair);
+        return this.unusedPairs.get(indexOfBestPair);
+    }
+
+    public void printSuggestedTestCases() {
+        System.out.println(this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "\n============== TEST CASES ==============\n" +
+                StringUtils.join(this.testCases, "\n");
+    }
+
+}
